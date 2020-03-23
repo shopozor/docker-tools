74bc392a5784bfa921ca326c5b813a26db13fcf7
Laurent Michel <laurent.michel@ruag.com>
2020-03-23 14:55:35 +0100

    add image with pickles

--- publish.sh
+++ publish.sh
@@ -7,3 +7,7 @@ docker push shopozor/devspace
 TOOL=docker-compose
 docker build -f $TOOL/Dockerfile --tag shopozor/$TOOL $TOOL
 docker push shopozor/$TOOL
+
+TOOL=pickles
+docker build -f $TOOL/Dockerfile --build-arg RELEASE_VERSION=2.20.1 --tag shopozor/$TOOL $TOOL
+docker push shopozor/$TOOL
