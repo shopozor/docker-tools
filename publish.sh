1d6ddba9afdaf62e84ca2d9ec23701714d8ffc53
Laurent Michel <laurent.michel@ruag.com>
2020-03-24 05:19:44 +0100

    add python lint tool

--- publish.sh
+++ publish.sh
@@ -11,3 +11,7 @@ docker push shopozor/$TOOL
 TOOL=pickles
 docker build -f $TOOL/Dockerfile --build-arg RELEASE_VERSION=2.20.1 --tag shopozor/$TOOL $TOOL
 docker push shopozor/$TOOL
+
+TOOL=python-lint
+docker build -f $TOOL/Dockerfile --tag shopozor/$TOOL $TOOL
+docker push shopozor/$TOOL
